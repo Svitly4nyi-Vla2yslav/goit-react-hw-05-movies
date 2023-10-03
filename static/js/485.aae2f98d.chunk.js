dc81f875e9cc7edaf02fe3cb3e942a977133947c
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-03 21:08:34 +0000

    Deploying to gh-pages from @ Svitly4nyi-Vla2yslav/goit-react-hw-05-movies@50d2476a7d0160437ed3afc1e6db950bce504aed 🚀

---
+++ static/js/485.aae2f98d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[485],{485:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});n(791);var r=n(184),i=function(){return(0,r.jsxs)("div",{children:["SearchMovie",(0,r.jsx)("input",{type:"text",name:"",id:""}),(0,r.jsx)("button",{type:"submit",children:"\ud83d\udd0d"})]})},u=function(){return(0,r.jsxs)("div",{children:["SearchMovie",(0,r.jsx)(i,{})]})}}}]);
+//# sourceMappingURL=485.aae2f98d.chunk.js.map
